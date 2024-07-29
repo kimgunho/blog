86f69971216cb15ceed3118f77b12060d3a0d665
kim <yuyukgh@naver.com>
2024-07-29 20:52:26 +0900

    change main card with tags

--- src/components/ui/Card/index.tsx
+++ src/components/ui/Card/index.tsx
@@ -13,9 +13,19 @@ interface Props {
 }
 
 const Card: React.FC<Props> = ({ data }) => {
+  const tags = data.properties.tags.multi_select.map((tag) => tag.name);
   return (
     <Link className={cx('container')} href={`/post/${data.id}`}>
-      <strong className={cx('title')}>{data.properties.title.title[0].plain_text}</strong>
+      <div className={cx('top')}>
+        <p className={cx('tags')}>
+          {tags.map((tag) => (
+            <span className={cx('tag')} key={tag}>
+              [{tag}]
+            </span>
+          ))}
+        </p>
+        <strong className={cx('title')}>{data.properties.title.title[0].plain_text}</strong>
+      </div>
       <p className={cx('summary')}>{data.properties.summary.rich_text[0]?.plain_text}</p>
       <p className={cx('updated')}>
         updated : {dayjs(data.properties.updatedAt.last_edited_time).format('YYYY.MM.DD')}
